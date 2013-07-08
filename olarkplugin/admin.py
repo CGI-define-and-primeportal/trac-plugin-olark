7fb3e75c6c996c455c1bca5976b295d99cf44b03
Danny Milsom <danny.milsom@cgi.com>
2013-07-08 14:13:31 +0000

    Copying trunk into my burn down feature branch
    git-svn-id: file:///mnt/ramdisk/d4-svn/branches/features/burndowncharts-584@13266 484df14f-75d2-4b3f-8ba0-490e05069a01

---
+++ olarkplugin/admin.py
@@ -0,0 +1,22 @@
+from trac.core import *
+from trac.admin import IAdminPanelProvider
+from trac.config import Option
+from trac.web.chrome import add_notice
+
+class OlarkAdmin(Component):
+    implements(IAdminPanelProvider)
+
+    # IAdminPanelProvider
+    
+    def get_admin_panels(self, req):
+        if req.perm.has_permission('TRAC_ADMIN'):
+            yield ('olark', 'Olark', 'olark', 'Olark')
+
+    def render_admin_panel(self, req, cat, page, path_info):
+        if req.method == 'POST':
+            self.config.set('olark', 'identity', req.args.get('identity'))
+            self.config.save()
+            add_notice(req, "Saved Olark identity")
+            req.redirect(req.href.admin(cat, page))
+                
+        return 'olark_admin.html', {'identity': self.config.get('olark','identity')}
